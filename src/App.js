320c3453dee8b3e05b540481ec4ff93b92a9784a
Matthew Phillips <45399355+Matthewmatical-99@users.noreply.github.com>
2019-05-19 17:44:01 -0400

    Revert "Updates"
    This reverts commit 3ccb124b9bbfa0039fceb4da3a73161394a1b117.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Sanik from './components/sanik';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" style={{ backgroundImage: 'url(https://imgur.com/kgbYeVO.jpg)' }}>
+        <p>More content coming soon. Please come back tomorrow.</p>
+        <a
+          className="App-link"
+          href="https://github.com/Matthewmatical-99"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My GitHub page
+        </a>
+        <Sanik size={200} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
