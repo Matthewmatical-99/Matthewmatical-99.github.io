1e72dde8234946031dfea7ece5cc9be2c06f02e0
Matthew Phillips <45399355+Matthewmatical-99@users.noreply.github.com>
2019-05-20 12:09:12 -0400

    Updates

---
+++ precache-manifest.c36c8db2655826621515fabbcc3d3be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d330656bc8cda882933329f7197deca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b5189275e309c996593",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "e198ca4e80075936bfb2",
+    "url": "/static/js/2.af3d9a3c.chunk.js"
+  },
+  {
+    "revision": "1b5189275e309c996593",
+    "url": "/static/js/main.4fc9584b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e071ee008610368e649c5733c6ddb59d",
+    "url": "/static/media/sanik.e071ee00.png"
+  }
+]);
--- src/components/sanik/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import sanikImage from '../../assets/sanik.png';
-import * as Styled from './styles';
-
-const Sanik = () => <Styled.Sanik src={sanikImage} />;
-
-export default Sanik;
